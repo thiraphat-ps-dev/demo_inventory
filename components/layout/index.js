d0e536be961492c8c8ad616c31cb4a16789102b9
gun <thiraphat.ps.dev@gmail.com>
2020-07-10 12:41:09 +0700

    pwa

--- components/layout/index.js
+++ components/layout/index.js
@@ -46,20 +46,40 @@ class Layout extends Component {
       <div style={{ position: 'relative' }}>
         <Head>
           <title>{title}</title>
-          <meta charSet="utf-8" />
+          <meta charset="utf-8" />
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
           <meta
             name="viewport"
-            content="initial-scale=1.0, width=device-width"
+            content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
           />
+          <meta name="description" content="Description" />
+          <meta name="keywords" content="Keywords" />
+          <title>Next.js PWA Example</title>
+
+          <link rel="manifest" href="/manifest.json" />
+          <link
+            href="/favicon-16x16.png"
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+          />
+          <link
+            href="/favicon-32x32.png"
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+          />
+          <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+          <meta name="theme-color" content="#317EFB" />
         </Head>
         {/* {checkLogin !== 'undefined' && checkLogin !== null ? ( */}
-          <div>
-            <Navbar />
-            <Sidebar />
-            <Menuoverlay />
+        <div>
+          <Navbar />
+          <Sidebar />
+          <Menuoverlay />
 
-            <div className="content-container active">{children}</div>
-          </div>
+          <div className="content-container active">{children}</div>
+        </div>
         {/* ) : null} */}
       </div>
     );
